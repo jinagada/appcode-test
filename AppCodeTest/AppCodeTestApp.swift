89a92fb97bba22794c27d71e4d26f475a826066d
jinagada <kimyg3174@gmail.com>
2021-02-10 15:22:31 +0900

    최초등록

---
+++ AppCodeTest/AppCodeTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppCodeTestApp.swift
+//  AppCodeTest
+//
+//  Created by Frodo on 2021/02/10.
+//
+//
+
+import SwiftUI
+
+@main
+struct AppCodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# AppCode Tutorials
+- AppCode를 사용한 IOS 앱 개발 테스트
+- URL : [Create a SwiftUI application in AppCode](https://www.jetbrains.com/help/objc/create-a-swiftui-application.html#step-1-create-a-project)
